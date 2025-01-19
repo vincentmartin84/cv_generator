240648f3cf7762f56c5a20af1034f15ecac37c7f
vincentmartin84 <vinmart@outlook.fr>
2025-01-19 10:36:35 +0100

    write all datas succesful

--- file_generator.py
+++ file_generator.py
@@ -1,15 +1,82 @@
 from docx import  Document
 
+#create new file
 def create_file():
     cv_doc = Document()
     return cv_doc
 
+#save the file
 def save_file(document, name_file):
     document.save(name_file )
 
 
-def write_contact_details(document, data):
-    names = document.add_paragraph(data["first_name"] + " " +  data["last_name"])
-    phone = document.add_paragraph(data["phone"])
-    email = document.add_paragraph(data["email"])
-    mention = document.add_paragraph(data["mention"])
+#create header Cv
+def header_cv(document, contact, title):
+
+    table = document.add_table(rows=1, cols=2)
+    cell_contact= table.cell(0,0)
+    cell_title= table.cell(0,1)
+
+    #write data cell contact
+
+    cell_contact.add_paragraph(contact["first_name"] + " " +  contact["last_name"])
+    cell_contact.add_paragraph("tel: " + contact["phone"])
+    cell_contact.add_paragraph("Email: " + contact["email"])
+    cell_contact.add_paragraph("Domicile: " + contact["residence"])
+    cell_contact.add_paragraph(contact["mention"])
+
+    #weite data cell title
+    cell_title.add_paragraph(title["title"])
+
+
+
+#trainings part
+def training_part(document, training):
+
+    #taining part title
+    training_title= document.add_paragraph("Formations")
+
+    #write trainings
+    for i, trn in enumerate(training,1):
+        document.add_paragraph(trn["title"] + " " + trn["school"] + " " + trn["date_start"] + ":" + trn["date_end"])
+#professionnal experiences
+def pro_experiences_part(document, experience):
+    #experiences title
+    exp_title= document.add_paragraph("Expériences professionnelles : ")
+
+    #weite experiences
+    for i, exp in enumerate(experience,1):
+        document.add_paragraph(exp["title"] + " " + exp["company"] + " " + exp["date_start"] + ":" + exp["date_end"] + ":" + exp["summary"])
+
+
+#technical skills
+def tech_skills(document, skills):
+
+    #technical skills title
+    skills_title = document.add_paragraph("Compétences techniques")
+    #write skills
+    document.add_paragraph(skills)
+
+#write professionnal skills
+def pro_skills(document, skills):
+
+    #professionnal skills title
+    pro_title= document.add_paragraph("Compétences professionnelles")
+
+    #write professionnal skills
+    document.add_paragraph(skills)
+
+
+
+#personal project
+def perso_projects(document, projects):
+
+    #personal project title
+    document.add_paragraph("Projets personnels")
+
+    #write personal projects
+    for i, prj in enumerate(projects,1):
+
+
+        project_label= document.add_paragraph(prj["title"]  + ": " +  prj["summary"])
+        project_link = document.add_paragraph(prj["link"])
--- main.py
+++ main.py
@@ -3,68 +3,53 @@ from cv_generator import  cv_title
 from cv_generator import collect_trainings
 from cv_generator import  collect_experiences
 from cv_generator import professionnal_skills
+from cv_generator import  technical_skills
 from cv_generator import personal_project
 from cv_generator import  collect_projects
 from file_generator import create_file
-from file_generator import  write_contact_details
 from file_generator import  save_file
+from file_generator import  header_cv
+from file_generator import training_part
+from file_generator import pro_experiences_part
+from file_generator import  tech_skills
+from file_generator import pro_skills
+from file_generator import perso_projects
 
+
+#create new file
 cv= create_file()
+"""
+#contact details
 contact = contact_details()
-ctc_details= write_contact_details(cv, contact)
-save_file(cv, 'mon_cv')
 
-#display data
-""" 
-contact = contact_details()
-print(contact["first_name"])
-print(contact["last_name"])
-print(contact["phone"])
+#title of CV
+title_cv= cv_title()
 
-print(contact["email"])
-print(contact["residence"])
-print(contact["mention"])
+#header cv
+header= header_cv(cv, contact,title_cv)
 
 
-cv = cv_title()
-print(cv["title"])
+#trainings part
+trn_list= collect_trainings()
+training_part(cv, trn_list)
 
-training= collect_trainings()
+#professionnal experiences part
+experiences= collect_experiences()
+pro_experiences_part(cv, experiences)
 
-for i, trn in enumerate(training, 1 ):
-    print(f"Formation {i}")
+#technical skills part
+tch_skills= technical_skills()
+tech_skills(cv, tch_skills)
 
-    print(trn["title"])
-    print(trn["school"])
-    print(trn["date_start"])
-    print(trn["date_end"])
-"""
-""" 
-experience = collect_experiences()
-for i, exp in enumerate(experience, 1):
-    print(f"Experience professionnelle {i}")
-
-    print(exp["title"])
-    print(exp["company"])
-    print(exp["date_start"])
-    print(exp["date_end"])
-    print(exp["summary"])
+#professionnal skills
+prf_skills = professionnal_skills()
+pro_skills(cv,prf_skills)
 """
 
-#tech_skills= technical_skills()
-#print(tech_skills)
-
-#pro_skills = professionnal_skills()
-#print(pro_skills)
+#personal project part
+prj_perso = collect_projects()
+perso_projects(cv, prj_perso)
 
 
-""" 
-projects= collect_projects()
-for i, prj in enumerate(projects, 1):
-    print(f"Projet personnel {i}")
-
-    print(prj["title"])
-    print(prj["summary"])
-    print(prj["link"])
-
-"""
+#save file
+save_file(cv, "my_cv")
